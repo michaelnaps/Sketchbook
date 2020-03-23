f72230c8f4cfb528fc426ccf0aa22f54afc0b59a
Michael Napoli <napoli.38@osu.edu>
2020-03-23 16:23:31 -0400

    no functional changes

--- MECHENG_2900/lab2/mapf_test.cpp
+++ MECHENG_2900/lab2/mapf_test.cpp
@@ -1,24 +1,34 @@
 /*
-// #include <iostream>
-// using namespace std;
+#include <iostream>
+using namespace std;
 
 int main()
 {
+   char cont('y');
    int val(0);
    float desired(0);
 
    // initial and final parameters
-   int init1(0), fin1(1023);
-   int init2(0), fin2(5);
+   int init1(0), fin1(0);
+   int init2(0), fin2(0);
+   
+   do {
+      cout << "Enter value: ";
+      cin >> val;
+      cout << "Enter initial range (2 values): ";
+      cin >> init1 >> fin1;
+      cout << "Enter new range (2 values): ";
+      cin >> init2 >> fin2;
 
-   cout << "Enter initial number (0-1023 range): ";
-   cin >> val;
+      desired = (float)val / (float)(fin1 - init1);
+      desired *= (float)(fin2 - init2);
+      desired += (float)init2;
 
-   desired = (float)val / (float)(fin1 - init1);
-   desired *= (float)(fin2 - init2);
-   desired += (float)init2;
-
-   cout << "The value in 0-5 range is " << desired << endl;
+      cout << "The new value range is " << desired << endl;
+      
+      cout << "Continue? (y/n) ";
+      cin >> cont;
+   } while (cont != 'n');
 
    return 0;
 }
