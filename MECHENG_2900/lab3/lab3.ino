a69168e51a5cb7e790ce5144d089275cf39b4596
michaelnaps <mdominicn@gmail.com>
2020-03-25 17:35:39 -0400

    updated comments lab 3

--- MECHENG_2900/lab3/lab3.ino
+++ MECHENG_2900/lab3/lab3.ino
@@ -42,6 +42,10 @@ void deactivate(const int& cyl) {
   digitalWrite((2 * cyl + 1), HIGH);  // open the exhaust valve
 }
 
+// function: calcRPM()
+// Function that returns the speed of a given rotating body
+// input: time in milliseconds
+// output: RPM speed
 float calcRPM(const int& millisec) {
   return (1 / ((float)millisec / (60000)));  // return RPM
 }
