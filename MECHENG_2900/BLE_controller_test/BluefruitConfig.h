4e13fe9d870a198cca874a779f7d6666fbfc3846
Michael Napoli <napoli.38@osu.edu>
2020-04-07 19:26:07 -0400

    Bluefruit testing file

--- MECHENG_2900/BLE_controller_test/BluefruitConfig.h
+++ MECHENG_2900/BLE_controller_test/BluefruitConfig.h
@@ -2,8 +2,9 @@
 // ----------------------------------------------------------------------------------------------
 // These settings are used in both SW UART, HW UART and SPI mode
 // ----------------------------------------------------------------------------------------------
-#define BUFSIZE                        160   // Size of the read buffer for incoming data
+#define BUFSIZE                        128   // Size of the read buffer for incoming data
 #define VERBOSE_MODE                   true  // If set to 'true' enables debug output
+#define BLE_READPACKET_TIMEOUT         500   // Timeout in ms waiting to read a response
 
 
 // SOFTWARE UART SETTINGS
@@ -14,7 +15,7 @@
 #define BLUEFRUIT_SWUART_RXD_PIN       9    // Required for software serial!
 #define BLUEFRUIT_SWUART_TXD_PIN       10   // Required for software serial!
 #define BLUEFRUIT_UART_CTS_PIN         11   // Required for software serial!
-#define BLUEFRUIT_UART_RTS_PIN         -1   // Optional, set to -1 if unused
+#define BLUEFRUIT_UART_RTS_PIN         8    // Optional, set to -1 if unused
 
 
 // HARDWARE UART SETTINGS
@@ -22,9 +23,9 @@
 // The following macros declare the HW serial port you are using. Uncomment
 // this line if you are connecting the BLE to Leonardo/Micro or Flora
 // ----------------------------------------------------------------------------------------------
-#ifdef Serial1    // this makes it not complain on compilation if there's no Serial1
-  #define BLUEFRUIT_HWSERIAL_NAME      Serial1
-#endif
+// #ifdef Serial1    // this makes it not complain on compilation if there's no Serial1
+//   #define BLUEFRUIT_HWSERIAL_NAME      Serial1
+// #endif
 
 
 // SHARED UART SETTINGS
@@ -41,9 +42,9 @@
 // using HW SPI.  This should be used with nRF51822 based Bluefruit LE modules
 // that use SPI (Bluefruit LE SPI Friend).
 // ----------------------------------------------------------------------------------------------
-#define BLUEFRUIT_SPI_CS               8
-#define BLUEFRUIT_SPI_IRQ              7
-#define BLUEFRUIT_SPI_RST              4    // Optional but recommended, set to -1 if unused
+// #define BLUEFRUIT_SPI_CS               8
+// #define BLUEFRUIT_SPI_IRQ              7
+// #define BLUEFRUIT_SPI_RST              -1    // Optional but recommended, set to -1 if unused
 
 // SOFTWARE SPI SETTINGS
 // ----------------------------------------------------------------------------------------------
