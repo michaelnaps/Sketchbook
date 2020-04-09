c0e1f23f546a67c9763e682b784a3d86d1718f73
Michael Napoli <napoli.38@osu.edu>
2020-04-08 21:56:24 -0400

    no notable changes

---
+++ MECHENG_2900/SART/SART.ino
@@ -0,0 +1,22 @@
+// Project: A.R.T. (Autonomous Robotic Toy)
+// Created by: Michael Napoli
+// Created on: 3/7/2020
+// Last modified on:
+
+/*
+ * Purpose: Main execution file for the A.R.T. decision making.
+*/
+
+const int trigPin(2);
+const int echoPin(4);
+
+void setup()
+{
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+}
+
+void loop()
+{
+  
+}
