eb76c2cc85fb00dabb907c7a42ef8c0f068a1cf9
Michael Napoli <napoli.38@osu.edu>
2020-05-25 13:23:24 -0400

    LCD testing incomplete

---
+++ LCDTest/LCDTest.ino
@@ -0,0 +1,26 @@
+#include <LiquidCrystal.h>
+
+LiquidCrystal lcd(1, 2, 4, 5, 6, 7);
+
+void setup() {
+  lcd.begin(16, 2);
+}
+
+void loop() {
+  lcd.print("Hello World");
+  delay(3000);
+
+  lcd.setCursor(2, 1);
+  lcd.print("LCD TEST");
+  delay(3000);
+
+  lcd.clear();
+
+  lcd.blink();
+  delay(4000);
+
+  lcd.setCursor(7, 1);
+  delay(3000);
+
+  lcd.noBlink();
+}
