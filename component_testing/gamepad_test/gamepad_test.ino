eb3da59f321bb4c105eda98495206c79a94e807d
michaelnaps <mdominicn@gmail.com>
2020-12-06 18:52:05 -0500

    joystick and buttons working together

--- component_testing/gamepad_test/gamepad_test.ino
+++ component_testing/gamepad_test/gamepad_test.ino
@@ -1,27 +1,30 @@
 // gamepad testing
 
+// Joystick pins
+const int x_js = A0;
+const int y_js = A1;
+
 // initialize buttons
 const int L = 12;
 const int a = 11;
 const int b = 10;
-const int x = 9;
-const int y = 8;
-
-// initialize joystick
-const int sw = 7;
+const int x_b = 9;
+const int y_b = 8;
 
 void setup() {
   pinMode(L, INPUT);
   pinMode(a, INPUT);
   pinMode(b, INPUT);
-  pinMode(x, INPUT);
-  pinMode(y, INPUT);
-  pinMode(sw, INPUT);
-  
+  pinMode(x_b, INPUT);
+  pinMode(y_b, INPUT); 
+
   Serial.begin(9600);
 }
 
 void loop() {
+  int x_dir = analogRead(x_js) - 500;
+  int y_dir = analogRead(y_js) - 500;
+  
   if (digitalRead(L) == HIGH)
     { Serial.println('L'); }
     
@@ -31,12 +34,22 @@ void loop() {
   if (digitalRead(b) == HIGH)
     { Serial.println('b'); }
     
-  if (digitalRead(x) == HIGH)
+  if (digitalRead(x_b) == HIGH)
     { Serial.println('x'); }
     
-  if (digitalRead(y) == HIGH)
+  if (digitalRead(y_b) == HIGH)
     { Serial.println('y'); }
-    
-  if (digitalRead(sw) == HIGH)
-    { Serial.println("sw"); }
+
+  if (x_dir > 50)
+    { Serial.println("+X"); }
+
+  if (x_dir < -50)
+    { Serial.println("-X"); }
+
+  if (y_dir > 50)
+    { Serial.println("+Y"); }
+
+  if (y_dir < -50)
+    { Serial.println("-Y"); }
+  delay(100);
 }
