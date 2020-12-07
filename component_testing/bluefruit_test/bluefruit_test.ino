5b240fae5c4dc4784975c7c13a4cdb7aed39802b
michaelnaps <mdominicn@gmail.com>
2020-12-07 00:29:26 -0500

    bluefruitLE still not initializing, going to switch to hardware serial and test

--- component_testing/bluefruit_test/bluefruit_test.ino
+++ component_testing/bluefruit_test/bluefruit_test.ino
@@ -1,24 +1,26 @@
 // testing of the Adafruit BluefruitLE component
 
 // Include Library for Bluefruit
-#include <SoftwareSerial.h>
-#include "Adafruit_BLE.h"
+#include <HardwareSerial.h>
+// #include "Adafruit_BLE.h"
 #include "Adafruit_BluefruitLE_UART.h"
 
 // Initialize BluefruitLE object
-SoftwareSerial mySerial(5, 6);
-Adafruit_BluefruitLE_UART btle(mySerial, -1, 3, -1);
+Adafruit_BluefruitLE_UART btle(Serial1, -1, 3, -1);
 
 void setup()
 {
+  Serial1.begin(9600);
   Serial.begin(9600);
 
   // establish BluefruitLE connection
-  if(!btle.begin()) {
+  if(!btle.begin(true)) {
     Serial.println("Could not initialize BluefruitLE."); 
     while(1);
   }
-  else { Serial.println("Initialization successful."); }
+  else { 
+    Serial.println("Initialization successful."); 
+  }
 
   // Factory reset BluefruitLE board
   if(!btle.factoryReset()) {
@@ -34,10 +36,14 @@ void setup()
     Serial.println("Name of module could not be changed.");
     while(1);
   }
-  else { Serial.println("New BluefruitLE Name: simple_controller"); }
+  else { Serial.println("New BluefruitLE Name: napoli_simple_controller"); }
+
+  btle.verbose(false);
 }
 
 void loop()
 {
+  // check/wait for connection
+  while(!btle.isConnected());
   
 }
