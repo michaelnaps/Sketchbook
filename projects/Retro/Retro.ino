36733fa36e548c8551ccb4ac122bdc8bed9688f3
Michael Napoli <napoli.38@osu.edu>
2020-12-14 23:02:00 -0500

    attempts to send characters to phone via BLE unsuccessful

--- projects/Retro/Retro.ino
+++ projects/Retro/Retro.ino
@@ -3,17 +3,21 @@
 // Created on: 11/11/2020
 // Last modified on: 11/11/2020
 
+/*
+ * Purpose: Programming controls for Bluetooth Low Energy
+ *  game controller. Program written for Arduino Nano 33 BLE
+ *  but should be adjustable to any Arduino BLE module that
+ *  can utlize the ArduinoBLE.h library.
+*/
+
 // Libaray for BLE services and characteristics
 #include <ArduinoBLE.h>
 
 // BLE services and characteristics
-BLEUnsignedIntCharacteristic hid_cp("0x2A4C", BLENotify);
-// BLEUnsignedIntCharacteristic hid_info("0x2A4A", BLENotify);
+const char id[] = "00002A4A-0000-1000-8000-00805f9b34fb";
+BLECharCharacteristic hid_cp(id, BLENotify);
 BLEService hid("00001812-0000-1000-8000-00805f9b34fb");
 BLEDevice cDevice;  // variable for central device information
-
-// create buffer variables for gamepad
-// byte buf[8] = {0};
 
 // function initializations (see definitions)
 void _successful();
@@ -54,7 +58,6 @@ void setup() {
 
   // set service and advertise as such
   hid.addCharacteristic(hid_cp);
-  // hid.addCharacteristic(hid_info);
   BLE.addService(hid);
   BLE.setAdvertisedService(hid);
 
@@ -72,13 +75,14 @@ void loop() {
   cDevice = BLE.central();
   while (!BLE.connected()) { BLE.advertise(); }
 
-  // check buttons and send data
-  keyCheck_dig(s, 15);
-  keyCheck_dig(a, 4);
-  keyCheck_dig(b, 5);
-  keyCheck_dig(xb, 27);
-  keyCheck_dig(yb, 28);
+  // check buttons and send data if applicable
+  keyCheck_dig(s, 's');
+  keyCheck_dig(a, 'a');
+  keyCheck_dig(b, 'b');
+  keyCheck_dig(xb, 'x');
+  keyCheck_dig(yb, 'y');
 
+  // check Joystick axis and send data if applicable
   keyCheck_an(xjs, 94, 92);
   keyCheck_an(yjs, 96, 90);
 }
--- projects/Retro/keyCheck_dig.ino
+++ projects/Retro/keyCheck_dig.ino
@@ -3,7 +3,7 @@
 // Purpose: check for button press and send to BLE device service
 // input: 'pin' - button pin
 //        'id' - key id to send to BLE
-void keyCheck_dig(const int& pin, const int& id)
+void keyCheck_dig(const int& pin, const char& id)
 {
   if (digitalRead(pin)) {
     // buf[2] = (byte)id;
